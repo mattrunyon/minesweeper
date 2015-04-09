99b4e4f48b7f5821c15c0e2385951b5c0a8b9149
Matthew Runyon <mattrunyon@tamu.edu>
2015-04-09 10:34:09 -0500

    Programatically display board

--- src/game.cpp
+++ src/game.cpp
@@ -1,15 +1,24 @@
 #include "imports/std_lib_facilities_4.h"
 #include <sstream>
+#include <FL/Fl.H>
+#include <FL/Fl_Box.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_JPEG_Image.H>
 #include <FL/Fl_Button.H>
 
 int main() {
-	Fl_Window win(16*9, 16*9, "Test");
+	Fl_Window win(100, 100, 16*9, 16*9, "Test");
 	win.begin();
-		Fl_Button but(10, 150, 70, 30, "Hi");
-		Fl_JPEG_Image("tileImages/coveredTile.jpg");
+	int x = 0;
+	int y = 0;
+	Fl_JPEG_Image tile("tileImages/coveredTile.jpg");
+	for (int x = 0; x < 16*9; x += 16) {
+		for (int y = 0; y < 16*9; y += 16) {
+			Fl_Button *box = new Fl_Button(x, y, 16, 16);
+			//box->image(tile);
+		}
+	}
 	win.end();
 	win.show();
-		
+	return (Fl::run());
 }
