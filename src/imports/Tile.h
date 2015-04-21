6936abf6681cd45bf51e3f86dd2d9486f017120c
Matthew Runyon <mattrunyon@tamu.edu>
2015-04-21 10:27:58 -0500

    Work on Tile.h

--- src/imports/Tile.h
+++ src/imports/Tile.h
@@ -1,15 +1,29 @@
 #include "std_lib_facilities_4.h"
+#include <FL/Fl_Button.H>
 
-class Tile {
-	bool hasMine;
+class Tile : Fl_Button {
+	bool containsMine = false;
 	bool beenClicked = false;
-	bool beenFlagged = false;
+	int numbRightClicks = 0;
+	int adjacentMines = 0;
+	
 	
 	public:
-		Tile(bool i);
+		void setMine(bool);
+		bool hasMine();
+		void incrementAdjacentMines();
 		
 };
 
-Tile::Tile(bool i) {
-	hasMine = i;
+void Tile::setMine(bool i) { 
+	this->containsMine = i;
 }
+
+bool Tile::hasMine() {
+	return this->containsMine;
+}
+
+void Tile::incrementAdjacentMines() {
+	this->adjacentMines += 1;
+}
+
